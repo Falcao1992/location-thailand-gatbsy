d21461ef1657151ae67d2b32ea009d1bef1cdfff
Falcao1992 <eduardo.marselle@gmail.com>
2020-04-18 16:27:41 +0200

    change name source file stysteme

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images/imageLocal`,
+                path: `${__dirname}/src/images/ImageLocal`,
             },
         },
         `gatsby-plugin-netlify`,
